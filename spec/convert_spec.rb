d536ca27f3af0fa8bf422fb1a1733c7b0baebc93
Jim Kingdon <jkingdon@localhost>
2012-10-20 23:20:46 -0700

    A few simple cases involving kind, var, and tvar.

--- spec/convert_spec.rb
+++ spec/convert_spec.rb
@@ -1,9 +1,43 @@
+# encoding: utf-8
+
 require 'rspec'
 require './lib/convert'
 
 describe "convert" do
+  def process(input)
+    Convert.new.process(StringIO.new(input))
+  end
+
   it "generates an empty file given an empty file" do
-    input = StringIO.new("")
-    Convert.new.process(input).should == ""
+    process("").should == ""
+  end
+
+  it "can convert kind to kind" do
+    process("kind (formula)").should == "kind (formula)"
+  end
+
+  it "converts var to tvar by default" do
+    process("kind (formula)\nvar (formula p)").should == "kind (formula)\ntvar (formula p)"
+  end
+
+  it "converts var to var for kind variable" do
+    process("kind (object)\nkind (variable)\nvar (variable x)").should ==
+      "kind (object)\nvar (object x)"
+  end
+
+  it "can convert a basic stmt" do
+    pending "not too close to getting this one working yet"
+    gh = process <<END
+kind (formula)
+var (formula p q)
+term (formula (→ formula formula))
+stmt (AntecedentIntroduction () () (p → (q → p)))
+END
+      gh.should == <<END
+kind (formula)
+tvar (formula p q)
+term (formula (→ p q))
+stmt (AntecedentIntroduction () () (→ p (→ q p)))
+END
   end
 end
