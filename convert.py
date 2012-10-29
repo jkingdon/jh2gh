5e2d3d9573eddec6542560d39819bb3b49bf3b79
Jim Kingdon <jkingdon@localhost>
2012-10-28 17:17:37 -0700

    Have convert preserve more whitespace.

--- convert.py
+++ convert.py
@@ -3,29 +3,23 @@ import tree
 
 class Convert:
   def convert(self, input):
-    result = ""
-    spacer = ""
     expressions = tree.parse(input)
     i = 0
     while i < expressions.count():
-      command = expressions[i]
-      arguments = expressions[i + 1]
+      command_index = i
+      argument_index = i + 1
       i += 2
 
-      if command == "kind" and arguments[0] == "variable":
-        continue
+      command = expressions[command_index]
+      arguments = expressions[argument_index]
 
-      if command == "var":
+      if command == "kind" and arguments[0] == "variable":
+        expressions[command_index] = '';
+        expressions[argument_index] = '';
+      elif command == "var":
         if arguments[0] == "variable":
           arguments[0] = "object"
         else:
-          command = "tvar"
-
-      result += spacer
-      result += command
-      result += " ("
-      result += arguments.to_string()
-      result += ")"
-      spacer = "\n"
-    return result
+          expressions[command_index] = "tvar"
+    return expressions.to_string()
 
--- test_convert.py
+++ test_convert.py
@@ -14,15 +14,18 @@ class test_convert(unittest.TestCase):
   def test_kind(self):
     self.assertEqual("kind (formula)", self.process("kind (formula)"))
 
-  def xtest_should_preserve_whitespace(self):
-    self.assertEqual("kind  \t\n(formula)", self.process("kind  \t\n(formula)"))
+  def test_should_preserve_whitespace_except_between_command_and_args(self):
+    self.assertEqual("\t\nkind  \t\n(   formula \t )\n\n",
+      self.process("\t\nkind  \t\n(   formula \t )\n\n"))
 
   def test_var_to_tvar(self):
     self.assertEqual("kind (formula)\ntvar (formula p)", self.process(
       "kind (formula)\nvar (formula p)"))
 
   def test_converts_var_to_var_for_kind_variable(self):
-    self.assertEqual("kind (object)\nvar (object x)", self.process(
+    # Might be kind of nice to clean up the whitespace, but maybe
+    # that isn't needed
+    self.assertEqual("kind (object)\n \nvar (object x)", self.process(
       "kind (object)\nkind (variable)\nvar (variable x)"))
 
 #  it "can convert a basic stmt" do
