3b67488c24598331b1af28fa02d1255502d59ca8
Jim Kingdon <jkingdon@localhost>
2012-12-03 18:08:06 -0800

    Tiny step towards being able to wiki-tokenize our input stream.

--- tokenizer.py
+++ tokenizer.py
@@ -1,15 +1,44 @@
+class WikiTokenizer:
+  def __init__(self, stream):
+    self._stream = stream
+    self._in_proof = False
+
+  def read_line_or_wiki_token(self):
+    while True:
+      line = self._stream.readline()
+      if line == '':
+        return line
+      elif line == "<jh>\n":
+        self._in_proof = True
+      elif line == "</jh>\n":
+        self._in_proof = False
+      elif self._in_proof:
+        return line
+      else:
+        return Wiki(line)
+
+class GhilbertTokenizer:
+  def __init__(self, stream):
+    self._stream = stream
+
+  def read_line_or_wiki_token(self):
+    return self._stream.readline()
+
 class Tokenizer:
   def __init__(self, stream):
-    self.stream = stream
+    self._wiki_tokenizer = GhilbertTokenizer(stream)
     self.line = ''
 
   def next_token(self):
     current_token = ''
     while True:
       if self.line == '':
-        self.line = self.stream.readline()
-        if self.line == '':
+        line_or_wiki_token = self._wiki_tokenizer.read_line_or_wiki_token()
+        if line_or_wiki_token == '':
           break
+        if line_or_wiki_token.__class__ == Wiki:
+          return line_or_wiki_token
+        self.line = line_or_wiki_token
 
       first = self.line[0]
       if first.isspace():
