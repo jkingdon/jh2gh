12dcb0125981b0472df33ebd8751bcf2a180c853
Jim Kingdon <jkingdon@localhost>
2012-12-28 23:39:32 -0700

    Add more pages to convert_all.py.

--- convert_all.py
+++ convert_all.py
@@ -9,7 +9,17 @@ class ConvertAll:
 
   def run(self):
     os.chdir('../wikiproofs')
+
+    self.convert('Interface:Axioms_of_intuitionistic_propositional_logic')
     self.convert('Intuitionistic_propositional_logic')
+    self.convert('Interface:Basic_intuitionistic_propositional_logic')
+    self.convert('Interface:Law_of_the_excluded_middle')
+    self.convert('From_intuitionistic_to_classical_propositional_logic')
+    self.convert('Interface:Principia_Mathematica_propositional_logic_theorems')
+    self.convert('Convenience_theorems_of_propositional_logic')
+    self.convert('Interface:Classical_propositional_calculus')
+
+
 
 if __name__ == '__main__':
 #  sys.stderr.write('Usage: python convert_all.py\n')
